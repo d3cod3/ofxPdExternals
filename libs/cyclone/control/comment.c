57a4a36b0bf361d943ce5ef4621b26816ab2e08f
d3cod3 <n3m3da@d3cod3.org>
2020-11-09 09:38:33 +0100

    switched to s_utf8.h from libpd to avoid symbol duplicates

--- libs/cyclone/control/comment.c
+++ libs/cyclone/control/comment.c
@@ -11,7 +11,7 @@
 // Porres cleaned up, rewrote and reorganized lots of thnigs and updated comment 2018-2020
 
 #include <common/api.h>
-#include <control/s_utf8.h>
+#include "s_utf8.h"
 #include "m_pd.h"
 #include "g_canvas.h"
 #include <string.h>
